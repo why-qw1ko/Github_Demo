aad01f5962cc207e94cdea1988044e9cdfa52b24
Buleng <2085886325@qq.com>
2023-03-26 13:03:48 +0800

    Idea测试提交GitHub仓库

---
+++ src/main/java/com/buleng/TokenApplication.java
@@ -0,0 +1,13 @@
+package com.buleng;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.buleng.mapper")
+public class TokenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TokenApplication.class,args);
+    }
+}
