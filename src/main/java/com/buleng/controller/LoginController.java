0e04cdfba3e7bec85312227d6e60a77bcab14845
Buleng <2085886325@qq.com>
2023-03-26 11:56:42 +0800

    Idea测试提交GitHub仓库

---
+++ src/main/java/com/buleng/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.buleng.controller;
+
+import com.buleng.domain.ResponseResult;
+import com.buleng.domain.User;
+import com.buleng.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class LoginController {
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping("/user/login")
+    public ResponseResult login(@RequestBody User user){
+        //登录
+        return loginService.login(user);
+    }
+
+    @RequestMapping("/user/logout")
+    public ResponseResult logout(){
+        return loginService.logout();
+    }
+}
---
+++ src/main/java/com/buleng/filter/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,62 @@
+package com.buleng.filter;
+
+import com.buleng.domain.LoginUser;
+import com.buleng.utils.JwtUtil;
+import com.buleng.utils.RedisCache;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private RedisCache redisCache;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //获取token
+        String token = request.getHeader("token");
+        if (!StringUtils.hasText(token)) {
+            //放行
+            filterChain.doFilter(request, response);
+            return;
+        }
+        //解析token
+        String userId;
+        try {
+            Claims claims = JwtUtil.parseJWT(token);
+            userId = claims.getSubject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("token非法");
+        }
+        //从redis中获取用户信息
+        String redisKey = "login:" + userId;
+        //通过redis的key获取
+        LoginUser loginUser = redisCache.getCacheObject(redisKey);
+        if (Objects.isNull(loginUser)) {
+            throw new RuntimeException("用户未登录");
+        }
+        //存入SecurityContextHolder
+        //TODO 获取权限信息封装到Authentication中
+        //TODO authorities为null，目前还没有权限信息，三个参数因为第三个参数代表已认证
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginUser, null, null);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        //放行
+        filterChain.doFilter(request, response);
+
+    }
+}
